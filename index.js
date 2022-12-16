9effd2e55f8f5169da7f6dc2b08a745d0dc61e6f
Michael Kiros <michael.kiros00@gmail.com>
2022-12-16 17:33:25 -0500

    commit tree c1419d3eafaf39a42a4adc80a6cb3b290a02bdef

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    const result = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(driversName => driversName.startsWith(string))
+    return result
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(hometown){
+        return hometown.name === string
+    })
+}
